cdbd3bd804c05bf76d44515465c02bf4e63d4694
fabio.yamaya <fabio.yamaya@gmail.com>
2023-05-16 21:40:32 -0300

    wtf am i doing

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Center, Heading } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <Center className="h-screen w-screen flex-col bg-rose-500">
+      <Heading className="text-white">wtf are u doing here</Heading>
+      <Heading className="text-white">wtf am i doing</Heading>
+    </Center>
+  );
+};
+
+export default Home;
