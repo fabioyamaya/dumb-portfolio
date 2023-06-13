d5cbe789be10738e9710bb63f007152c5b3d9108
fabio.yamaya <fabio.yamaya@gmail.com>
2023-06-13 19:39:30 -0300

    add animation to home segment elements

--- components/HomeSegment/HomeSegment.tsx
+++ components/HomeSegment/HomeSegment.tsx
@@ -1,5 +1,43 @@
-import { Flex, Heading, Icon, SimpleGrid, Text } from "@chakra-ui/react";
+import { Flex, Icon, SimpleGrid, Text } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import React from "react";
 import { HiChevronDoubleDown } from "react-icons/hi";
+
+const variants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: { staggerChildren: 0.2 },
+  },
+};
+
+const item = {
+  hidden: {
+    opacity: 0,
+    y: +70,
+    atrrY: +70,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    attrY: 0,
+    transition: {
+      duration: 1,
+      ease: [0.2, 0.59, 0.13, 0.94],
+    },
+  },
+};
+
+const MotionChakraIcon = motion(
+  React.forwardRef<HTMLDivElement>(function ChevronDownIcon(_props, ref) {
+    return (
+      <div ref={ref}>
+        <Icon as={HiChevronDoubleDown} boxSize={"10"} />
+      </div>
+    );
+  }),
+  { forwardMotionProps: true }
+);
 
 const HomeSegment = () => {
   return (
@@ -7,20 +45,36 @@ const HomeSegment = () => {
       column={2}
       className="mx-36 my-auto flex h-full w-1/2 content-center "
     >
-      <Flex className="min-h-[500px] flex-col items-start justify-between">
+      <Flex
+        as={motion.div}
+        variants={variants}
+        initial="hidden"
+        animate="show"
+        className="min-h-[500px] flex-col items-start justify-between"
+      >
         <Flex className="flex-col items-end">
-          <Heading as="h1" size={"4xl"}>
+          <motion.h1 variants={item} className="text-7xl font-bold">
             Hey, I&apos;m Fabio Yamaya
-          </Heading>
-          <Text className="font-thin text-slate-500" fontSize={"2xl"} as="i">
+          </motion.h1>
+          <Text
+            as={motion.i}
+            variants={item}
+            className="font-thin text-slate-500"
+            fontSize={"2xl"}
+          >
             I do some frontend stuff
           </Text>
         </Flex>
-        <Text className="font-thin" fontSize={"5xl"}>
+        <Text
+          as={motion.p}
+          variants={item}
+          className="font-thin"
+          fontSize={"5xl"}
+        >
           and this is my take <br />
           on a dumb portfolio
         </Text>
-        <Icon as={HiChevronDoubleDown} boxSize={"10"} />
+        <MotionChakraIcon variants={item} />
       </Flex>
     </SimpleGrid>
   );
